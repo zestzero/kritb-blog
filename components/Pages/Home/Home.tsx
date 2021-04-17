d7063e11a306e52af2a85944dac14ffb0e776a82
Krit Bannachaisirisuk <gmkipladen@gmail.com>
2021-04-17 20:51:04 +0700

    first commit

---
+++ components/Pages/Home/Home.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent } from "react";
+
+interface Props {
+  isSSR: boolean;
+  isStatic: boolean;
+}
+
+export const Home: FunctionComponent<Props> = (props) => {
+  return (
+    <div>
+      <div>{props.isSSR ? <h2>SSR Working</h2> : <h2>SSR Not Works</h2>}</div>
+
+      <div>
+        {props.isStatic ? <h2>Static generated</h2> : <h2>is Not Static</h2>}
+      </div>
+    </div>
+  );
+};
